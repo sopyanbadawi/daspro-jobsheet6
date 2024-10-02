64ba1946ecb59f4567e8ce20e68012bc3a784362
sopyanbadawi <sopyanbadawi.com>
2024-10-02 11:14:36 +0700

    Cek Tahun 2

--- Pemilihan2Percobaan104.java
+++ Pemilihan2Percobaan104.java
@@ -10,7 +10,8 @@ public class Pemilihan2Percobaan104 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
-            }
+            } else
+            System.out.println("Bukan Tahun Kabisat");
         } else
             System.out.println("Bukan Tahun Kabisat");
     }
