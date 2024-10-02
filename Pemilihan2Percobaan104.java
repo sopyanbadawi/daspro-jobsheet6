9331959d080df51cf56c658c53f850925c9a6c2f
sopyanbadawi <sopyanbadawi.com>
2024-10-02 10:27:17 +0700

    Cek Tahun 1

--- Pemilihan2Percobaan104.java
+++ Pemilihan2Percobaan104.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan104 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int tahun;
+        int tahun04;
         
         System.out.print("Masukkan Tahun: ");
-        tahun = sc.nextInt();
+        tahun04 = sc.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
+        if ((tahun04 % 4) == 0) {
+            if ((tahun04 % 100) != 0) {
                 System.out.println("Tahun Kabisat");
             }
         } else
             System.out.println("Bukan Tahun Kabisat");
     }
-    
 }
