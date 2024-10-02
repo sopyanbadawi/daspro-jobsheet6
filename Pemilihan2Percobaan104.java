811d237b6d19b95e37f638111a81adcc26a80b1f
sopyanbadawi <sopyanbadawi.com>
2024-10-02 11:41:10 +0700

    Cetak Tahun 3

--- Pemilihan2Percobaan104.java
+++ Pemilihan2Percobaan104.java
@@ -10,7 +10,9 @@ public class Pemilihan2Percobaan104 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
-            } else
+            } else if ((tahun % 400) == 0) {
+                System.out.println("Tahun Kabisat");
+            } else 
             System.out.println("Bukan Tahun Kabisat");
         } else
             System.out.println("Bukan Tahun Kabisat");
